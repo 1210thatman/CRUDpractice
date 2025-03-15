f3d834fbd3de7ed51d76bd908ebf423724162d77
unknown <1210thatman@gmail.com>
2025-03-15 18:00:53 +0900

    feat: BoardUpdateReques 생성

---
+++ CRUDpractice/src/main/java/org/example/crudpractice/domain/board/persistence/dto/request/BoardUpdateRequest.java
@@ -0,0 +1,10 @@
+package org.example.crudpractice.domain.board.persistence.dto.request;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class BoardUpdateRequest {
+    private String boardName;
+}
