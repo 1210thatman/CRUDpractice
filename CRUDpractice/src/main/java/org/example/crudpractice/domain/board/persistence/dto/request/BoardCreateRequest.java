aa58b304ca1b7cd93949ee7cf47f94a109cb3552
unknown <1210thatman@gmail.com>
2025-03-15 13:47:48 +0900

    feat: boardRequest 추가

--- CRUDpractice/src/main/java/org/example/crudpractice/domain/board/persistence/dto/request/BoardCreateRequest.java
+++ CRUDpractice/src/main/java/org/example/crudpractice/domain/board/persistence/dto/request/BoardCreateRequest.java
@@ -6,5 +6,4 @@ import lombok.Getter;
 public class BoardCreateRequest {
     private String title;
     private String adminName;
-    private int  amountOfPost;
 }
---
+++ CRUDpractice/src/main/java/org/example/crudpractice/domain/board/persistence/dto/response/BoardResponse.java
@@ -0,0 +1,4 @@
+package org.example.crudpractice.domain.board.persistence.dto.response;
+
+public class BoardResponse {
+}
